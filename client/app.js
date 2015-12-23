1107814f63f23c0f7d4fbd90d9c1ca69fd2d46d4
Hector Flores <hecto932@gmail.com>
2015-12-23 18:29:30 -0430

    Soporte browserify y permitir grabar video en el cliente

---
+++ client/app.js
@@ -0,0 +1,25 @@
+const Webrtc2Images = require('webrtc2images')
+
+const rtc = new Webrtc2Images({
+	width: 200,
+	height: 200,
+	frames: 10,
+	type: 'image/jpeg',
+	quality: 0.4,
+	interval: 200
+})
+
+rtc.startVideo(function(err){
+
+})
+
+const record = document.querySelector('#record')
+
+
+record.addEventListener('click', function(){
+	e.preventDefault()
+
+	rtc.recordVideo(function(err, frames){
+		console.log(frames)
+	})
+}, false)
