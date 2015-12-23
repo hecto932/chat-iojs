f385d6b7046203680f79b8fd549fc673ac487170
Hector Flores <hecto932@gmail.com>
2015-12-23 11:59:47 -0430

    Creamos un servidor web estatico muy basico

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use_strict'
 
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const port = process.env.PORT || 8080
 
 const server = http.createServer();
@@ -8,12 +10,18 @@ const server = http.createServer();
 server.on('request', onRequest)
 server.on('listening', onListening)
 
-server.listen(port);
+server.listen(port)
 
 function onRequest(req, res){
-	res.end('Hello io.js')
+	var fileName = path.join(__dirname, 'public', 'index.html')
+	fs.readFile(fileName, function(err, file){
+		if (err){
+			return res.end(err.message)
+		}
+		res.end(file)
+	})
 }
 
 function onListening(){
-	console.log('Servidor escuchando en puerto ' + port)
+	console.log('http://localhost:'+port+'/')
 }
