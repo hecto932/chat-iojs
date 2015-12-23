98c6b1e6cc52214621166619c51045ef944238a8
Hector Flores <hecto932@gmail.com>
2015-12-23 11:06:29 -0430

    Agregar callback en el request handle y en el listen

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const http = require('http')
 const port = process.env.PORT || 8080
 
-const server = http.createServer()
+const server = http.createServer(function (req, res) {
+	res.end('Hola io.js')
+});
 
-server.listen(port)
+server.listen(port, function(){
+	console.log('Servidor escuchando en puerto' + port)
+});
