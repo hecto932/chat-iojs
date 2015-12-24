efd928abee5dfddf20ac0c4fc90a58d6e31b77db
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-12-23 19:49:56 -0430

    Agregar ruta /process y enviar imagenes para procesamiento

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+const xhr = require('xhr')
 const Webrtc2Images = require('webrtc2images')
 
 const rtc = new Webrtc2Images({
@@ -20,6 +21,16 @@ record.addEventListener('click', function(e){
 	e.preventDefault()
 
 	rtc.recordVideo(function(err, frames){
-		console.log(frames)
+		if(err) return logError(err)
+
+		xhr({
+			uri: '/process',
+			method: 'post',
+			header: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ images: frames })
+		}, function(err, res, body){
+			if(err) return logError(err)
+			console.log(JSON.parse(body))
+		})
 	})
 }, false)
