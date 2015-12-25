aee1a8d7a75d5d997429350a47eaf40dbcfac4eb
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-12-25 03:29:24 -0430

    Funcionando

--- public/app.js
+++ public/app.js
@@ -3,37 +3,45 @@ const xhr = require('xhr')
 const Webrtc2Images = require('webrtc2images')
 
 const rtc = new Webrtc2Images({
-	width: 200,
-	height: 200,
-	frames: 10,
-	type: 'image/jpeg',
-	quality: 0.4,
-	interval: 200
+  width: 200,
+  height: 200,
+  frames: 10,
+  type: 'image/jpeg',
+  quality: 0.4,
+  interval: 200
 })
 
-rtc.startVideo(function(err){
-
+rtc.startVideo(function (err) {
+  if (err) return logError(err)
 })
 
 const record = document.querySelector('#record')
 
+record.addEventListener('click',  function (e) {
+  e.preventDefault()
 
-record.addEventListener('click', function(e){
-	e.preventDefault()
+  rtc.recordVideo(function (err, frames) {
+    if (err) return logError(err)
 
-	rtc.recordVideo(function(err, frames){
-		if(err) return logError(err)
+    xhr({
+      uri: '/process',
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ images: frames }),
+    }, function (err, res, body) {
+      if (err) return logError(err)
 
-		xhr({
-			uri: '/process',
-			method: 'post',
-			header: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ images: frames })
-		}, function(err, res, body){
-			if(err) return logError(err)
-			console.log(JSON.parse(body))
-		})
-	})
+      body = JSON.parse(body)
+
+      if (body.video) {
+        const video = document.querySelector('#video')
+        video.src = body.video
+        video.loop = true
+        video.play()
+      }
+    })
+
+  })
 }, false)
 
 function logError (err) {
